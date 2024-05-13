2e1354ed638bb56ba4394e7c881a84da563d9916
Your Name <you@example.com>
2024-05-13 12:25:34 -0300

    se arregló la consigna

--- assets/js/iteracionNumeros.js
+++ assets/js/iteracionNumeros.js
@@ -1,14 +1,5 @@
 let num = 0
 
-// while (num <10){
-//     if (num%2==0) {
-//         console.log(num,"es par");
-//     }else{
-//         console.log(num,"es impar");
-//     }
-//     num ++
-// }
-
 for ( num = 0; num <= 10; num++) {
     if (num%2==0) {
         console.log(num,"es par");
